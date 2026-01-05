df092d3a7d13d6d86f37127a98b8e83c702c4159
noyeosa-obazee <obazeenoyeosa@gmail.com>
2026-01-05 10:15:39 +0100

    prepare for deployment

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use("/", indexRoute);
 
